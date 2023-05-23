7deda2187c1b7dc8a62cd824b00dfc144371ee03
jacques Mouafo Tagu <jacques_fabrice32@yahoo.fr>
2023-05-24 00:47:30 +0200

    Added tailwind and configured prettier

---
+++ client/tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,ts}'],
+  theme: {
+    extend: {},
+    colors: {
+      white: '#fefcfb',
+      whitish: '#FFFAFF',
+      blue: '#0496FF',
+      blue2: '#072ac8',
+      orange: '#FF9F1C',
+      rose: '#E71D36',
+      mustard: '#fcdc4d',
+      pumpkin: '#FE7F2D',
+      black: '#011627',
+      darkBrown: '#FFFAFF',
+      green: '#717744',
+    },
+    fontFamily: {
+      poppins: 'Poppins',
+      shrikland: 'Shrikland',
+      fasthand: 'Fasthand',
+    },
+  },
+  plugins: [],
+};
